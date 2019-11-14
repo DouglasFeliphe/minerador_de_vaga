c9a5012d0f342173ad1c94ab80e66969cdad1835
Thiago Neves <thiagoneves242@gmail.com>
2019-11-14 13:16:31 +0000

    puxando o código da vaga

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import time 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
 import logging
 import json
 
-url = 'https://hipsters.jobs/jobs/?q=&l=Bras%C3%ADlia+-+Brasilia%2C+Federal+District%2C+Brazil'
+
+# url = 'https://hipsters.jobs/jobs/?q=&l=Bras%C3%ADlia+-+Brasilia%2C+Federal+District%2C+Brazil'
+url = 'https://hipsters.jobs/jobs/?l=Bras%C3%ADlia%20-%20Brasilia%2C%20Federal%20District%2C%20Brazil&p=2'
 soup = BeautifulSoup(urlopen(url), "html.parser")
 
 #puxa_titulo
@@ -12,28 +15,58 @@ titulo = []
 for item in soup.select('.listing-item__title'):
   texto = item.get_text().strip()
   titulo.append(texto)
-titulo
+# titulo
+
+# puxa código
+codigo = []
+for item in soup.select('.listing-item__title'):
+  titulo_vaga = item.get_text().strip()
+  numeros = ''
+  if(titulo_vaga.find('Cod') > -1 or titulo_vaga.find('Cód') > -1):
+    letras = titulo_vaga.rstrip('0123456789')
+    numeros = titulo_vaga[len(letras):]    
+  else:
+    numeros = 'sem código'
+  # print(numeros)  
+  codigo.append(numeros)
  
 #puxa link
 links = []
 for item in soup.select('.listing-item__title'):
   links.append(item.a.get('href'))
-links
+
 
 # puxa data
 data = []
 for item in soup.select('.listing-item__date'):
 	data.append(item.get_text().strip())
-data
+# data
 
-# for i in range(0,len(lista_vagas)):
-#     lista_vagas = {data: (titulo[i], links[i])} 
-#     print(lista_vagas)
-    
+# puxa empresa
+empresa = []
+for item in soup.select('.listing-item__info--item-company'):
+  texto = item.get_text().strip()
+  empresa.append(texto)
+# empresa
+ 
+ #puxa localização
+locale = []
+for item in soup.select('.listing-item__info--item-location'):
+  texto = item.get_text().strip()
+  locale.append(texto)
 
+#puxador de tipo de contratação
+tipo_contratacao = []
+for item in soup.select('.listing-item__employment-type'):
+  texto = item.get_text().strip()
+  tipo_contratacao.append(texto)
+# print(tipo_contratacao)
 
-
-
+# puxa descricao previa da vaga
+desc_vaga = []
+for item in soup.select('.visible-xs'):
+  texto = item.get_text().strip()
+  desc_vaga.append(texto)
 
 # salvar o dicionário gerado em um arquivo JSON
 # dicionario_exemplo = {
@@ -48,9 +81,6 @@ data
 # arquivo_json = open(dicionario_json,'x')
 # arquivo_json.write(dicionario_exemplo)
 # arquivo_json.close()
-
-
-
-
+ 
 
 
