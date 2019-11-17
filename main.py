9ec3bcc4c7c39f97b6550d4361f61e0b1b93620e
DouglasFeliphe <douglasfeliphe@hotmail.com>
2019-11-17 21:50:25 -0200

    salvar o dicionário gerado em um arquivo JSON

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import time 
+import time
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 # import requests
 # import logging
-# import json
-
+import os.path
+import json
 
 # url = 'https://hipsters.jobs/jobs/?q=&l=Bras%C3%ADlia+-+Brasilia%2C+Federal+District%2C+Brazil'
 url = 'https://hipsters.jobs/jobs/?l=Bras%C3%ADlia%20-%20Brasilia%2C%20Federal%20District%2C%20Brazil&p=2'
@@ -14,78 +14,79 @@ soup = BeautifulSoup(urlopen(url), "html.parser")
 titulo = []
 codigo = []
 for item in soup.select('.listing-item__title'):
-  titulo_vaga = item.get_text().strip()
-  numeros = ''
+    titulo_vaga = item.get_text().strip()
+    numeros = ''
 
-  # se título possui código...
-  if(titulo_vaga.find(':') > -1):
+    # se título possui código...
+    if (titulo_vaga.find(':') > -1):
 
-    # extraindo o título e o código
-    texto = slice(0, titulo_vaga.find(':')-4, 1) #título
-    numeros = slice((titulo_vaga.find(':')+2), 99, 1) #código
+        # extraindo o título e o código
+        texto = slice(0, titulo_vaga.find(':') - 4, 1)  # título
+        numeros = slice((titulo_vaga.find(':') + 2), 99, 1)  # código
 
-    titulo.append(titulo_vaga[texto])
-    codigo.append(titulo_vaga[numeros].strip())
+        titulo.append(titulo_vaga[texto])
+        codigo.append(titulo_vaga[numeros].strip())
 
-  else:
-    numeros = 'sem código'
-    titulo.append(titulo_vaga)
-    codigo.append(numeros)
- 
-#puxa link
-links = []
+    else:
+        numeros = 'sem código'
+        titulo.append(titulo_vaga)
+        codigo.append(numeros)
+
+# puxa descricao prévia da vaga
+descricao_da_vaga = []
+for item in soup.select('.visible-xs'):
+    texto = item.get_text().strip()
+    descricao_da_vaga.append(texto)
+
+# puxa link
+link = []
 for item in soup.select('.listing-item__title'):
-  links.append(item.a.get('href'))
+    link.append(item.a.get('href'))
 
 # puxa data
 data = []
 for item in soup.select('.listing-item__date'):
-	data.append(item.get_text().strip())
+    data.append(item.get_text().strip())
 # data
 
 # puxa empresa
 empresa = []
 for item in soup.select('.listing-item__info--item-company'):
-  texto = item.get_text().strip()
-  empresa.append(texto)
-# empresa
- 
- #puxa localização
-locale = []
+    texto = item.get_text().strip()
+    empresa.append(texto)
+
+# puxa localização
+localizacao = []
 for item in soup.select('.listing-item__info--item-location'):
-  texto = item.get_text().strip()
-  locale.append(texto)
+    texto = item.get_text().strip()
+    localizacao.append(texto)
 
-#puxador de tipo de contratação
-tipo_contratacao = []
+# puxador de tipo de contratação
+tipo_de_contratacao = []
 for item in soup.select('.listing-item__employment-type'):
-  texto = item.get_text().strip()
-  tipo_contratacao.append(texto)
-# print(tipo_contratacao)
+    texto = item.get_text().strip()
+    tipo_de_contratacao.append(texto)
 
-# puxa descricao previa da vaga
-desc_vaga = []
-for item in soup.select('.visible-xs'):
-  texto = item.get_text().strip()
-  desc_vaga.append(texto)
-
-for i in codigo:
-  print(i)
-
+# criando dicionário
+lista_de_vagas = []
+for i in range(0, len(titulo)):
+    vaga = {
+        data[i]: (titulo[i],
+                  codigo[i],
+                  descricao_da_vaga[i],
+                  link[i],
+                  tipo_de_contratacao[i],
+                  empresa[i],
+                  localizacao[i],
+                  )
+    }
+    lista_de_vagas.append(vaga)
 
 # salvar o dicionário gerado em um arquivo JSON
-# dicionario_exemplo = {
-#   'id': 01,
-#   'nome': 'dic_ex'
-# }
 
-# convertendo o dicionário para json
-# dicionario_json = json.dumps(dicionario_exemplo)
-
-# criando o arquivo json
-# arquivo_json = open(dicionario_json,'x')
-# arquivo_json.write(dicionario_exemplo)
-# arquivo_json.close()
- 
-
-
+arquivo_json = 'vagas.json'
+#cria o arquivo json
+dados_json = json.dumps(lista_de_vagas, ensure_ascii=False, indent=2) # converte o dicionário para json
+arquivo = open(arquivo_json, 'w')
+arquivo.write(dados_json) # salva o dicionário no arquivo json
+arquivo.close()
